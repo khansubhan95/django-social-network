f1c7c1e9631300a873b778c2d84e4c3f53a659a8
Mohammed Subhan Khan <khansubhan95@gmail.com>
2019-06-14 15:54:53 -0500

    Remove print from settings

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -15,7 +15,6 @@ import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 with open(os.path.join(BASE_DIR, 'social_network/secrets.json')) as f: 
     secrets = json.loads(f.read())
